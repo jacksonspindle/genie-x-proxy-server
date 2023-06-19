ddba4a5566bbde70a5e79badd27b6fe01554684b
Jackson Spindle <jacksonmilesspindle@gmail.com>
2023-06-19 14:30:46 -0600

    added proxy server code

---
+++ functions/server.js
@@ -0,0 +1,39 @@
+const axios = require("axios");
+const fs = require("fs");
+const path = require("path");
+
+exports.handler = async (event) => {
+  const { imageUrl } = event.queryStringParameters;
+
+  const imagesDirectory = path.join(__dirname, "src", "assets");
+  if (!fs.existsSync(imagesDirectory)) {
+    fs.mkdirSync(imagesDirectory);
+  }
+
+  try {
+    const imageResponse = await axios.get(imageUrl, { responseType: "stream" });
+
+    const fileName = "downloaded-image.jpg";
+    const filePath = path.join(imagesDirectory, fileName);
+
+    const writeStream = fs.createWriteStream(filePath);
+    imageResponse.data.pipe(writeStream);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        success: true,
+        message: "Image downloaded successfully.",
+      }),
+    };
+  } catch (error) {
+    console.error("Error while downloading the image:", error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        success: false,
+        message: "Failed to download the image.",
+      }),
+    };
+  }
+};
