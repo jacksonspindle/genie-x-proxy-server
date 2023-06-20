e762db207f1b874d703027202d898c4e896fcec0
Jackson Spindle <jacksonmilesspindle@gmail.com>
2023-06-19 20:24:15 -0600

    bug fix

---
+++ functions/image-proxy.js
@@ -0,0 +1,24 @@
+const axios = require("axios");
+
+exports.handler = async (event, context) => {
+  try {
+    const response = await axios.get(event); // Replace with the actual DALLE API endpoint
+
+    const headers = {
+      "Content-Type": "image/jpeg",
+      "Access-Control-Allow-Origin": "*", // Adjust the CORS policy as needed
+    };
+
+    return {
+      statusCode: 200,
+      headers,
+      body: response.data,
+    };
+  } catch (error) {
+    console.error("Error retrieving image:", error);
+    return {
+      statusCode: 500,
+      body: "Internal Server Error",
+    };
+  }
+};
