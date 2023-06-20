2e4a45d4c886be03c17a3b708a71b7f9b665e564
Jackson Spindle <jacksonmilesspindle@gmail.com>
2023-06-19 20:34:57 -0600

    bug fix

--- functions/image-proxy.js
+++ functions/image-proxy.js
@@ -2,17 +2,23 @@ const axios = require("axios");
 
 exports.handler = async (event, context) => {
   try {
-    const response = await axios.get(event); // Replace with the actual DALLE API endpoint
+    // Retrieve image from DALLE API
+    const response = await axios.get(event); // Use the event directly as the URL
 
     const headers = {
       "Content-Type": "image/jpeg",
       "Access-Control-Allow-Origin": "*", // Adjust the CORS policy as needed
     };
 
+    // Download the image
+    const imageData = response.data;
+
+    // Send the image back to the frontend React app
     return {
       statusCode: 200,
       headers,
-      body: response.data,
+      body: imageData,
+      isBase64Encoded: true,
     };
   } catch (error) {
     console.error("Error retrieving image:", error);
