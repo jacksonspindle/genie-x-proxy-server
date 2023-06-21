e17e0054148d57e2850da16aa623684ed95246eb
Jackson Spindle <jacksonmilesspindle@gmail.com>
2023-06-21 17:32:35 -0600

    added a second server function for retrieving collection

---
+++ functions/retrieve-collection.js
@@ -0,0 +1,40 @@
+const axios = require("axios");
+
+exports.handler = async function (event, context) {
+  try {
+    const { urls } = JSON.parse(event.body);
+
+    const base64Images = [];
+
+    for (const url of urls) {
+      const response = await axios.get(url, {
+        responseType: "arraybuffer",
+      });
+
+      const base64Image = Buffer.from(response.data, "binary").toString(
+        "base64"
+      );
+      const imageData = `data:${response.headers["content-type"]};base64,${base64Image}`;
+
+      base64Images.push(imageData);
+    }
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ base64Images }),
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*", // Allow cross-origin requests
+      },
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Failed to download images" }),
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*", // Allow cross-origin requests
+      },
+    };
+  }
+};
