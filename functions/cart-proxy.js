c84478e02f7dec30c83158f1b45124f05a831fa3
Jackson Spindle <jacksonmilesspindle@gmail.com>
2023-10-03 17:31:58 -0600

    bug fix

--- functions/cart-proxy.js
+++ functions/cart-proxy.js
@@ -3,12 +3,16 @@ const axios = require("axios");
 exports.handler = async function (event, context) {
   try {
     const imageUrl = event.queryStringParameters.imageUrl;
+    console.log("Fetching image from:", imageUrl); // Log the URL being fetched
+
     const response = await axios.get(imageUrl, {
       responseType: "arraybuffer",
     });
 
     const base64Image = Buffer.from(response.data, "binary").toString("base64");
     const imageData = `data:${response.headers["content-type"]};base64,${base64Image}`;
+
+    console.log("Image fetched successfully");
 
     return {
       statusCode: 200,
@@ -19,6 +23,8 @@ exports.handler = async function (event, context) {
       },
     };
   } catch (error) {
+    console.error("Error fetching image:", error); // Log any errors
+
     return {
       statusCode: 500,
       body: JSON.stringify({ error: "Failed to download image" }),
