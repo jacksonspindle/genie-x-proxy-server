f94ba6058480f2d91d0539f9c97e825f81654a08
Jackson Spindle <jacksonmilesspindle@gmail.com>
2023-10-09 13:10:57 -0600

    bug fix

--- functions/cart-proxy.js
+++ functions/cart-proxy.js
@@ -2,21 +2,23 @@ const axios = require("axios");
 
 exports.handler = async function (event, context) {
   try {
-    const imageUrl = event.queryStringParameters.imageUrl; // Directly use the imageUrl without decoding
-    const token = event.queryStringParameters.token; // Get the token parameter
+    const imageUrl = event.queryStringParameters.imageUrl;
+    const token = event.queryStringParameters.token;
 
     // Construct the image URL using the provided parameters
-    const imageUrlWithToken = `${imageUrl}&token=${token}`;
+    let imageUrlWithToken = imageUrl;
+    if (token && token !== "undefined") {
+      imageUrlWithToken += `&token=${token}`;
+    }
 
     console.log(imageUrlWithToken);
     console.log("event:", event);
     console.log("event raw url:", event.rawUrl);
     console.log("imageUrl", imageUrl);
 
-    console.log("Fetching image from:", imageUrlWithToken); // Log the URL being fetched
+    console.log("Fetching image from:", imageUrlWithToken);
 
     const response = await axios.get(imageUrlWithToken, {
-      // Use imageUrlWithToken instead of event.rawUrl
       responseType: "arraybuffer",
     });
 
@@ -30,18 +32,18 @@ exports.handler = async function (event, context) {
       body: JSON.stringify({ imageUrl: imageData }),
       headers: {
         "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*", // Allow cross-origin requests
+        "Access-Control-Allow-Origin": "*",
       },
     };
   } catch (error) {
-    console.error("Error fetching image:", error); // Log any errors
+    console.error("Error fetching image:", error);
 
     return {
       statusCode: 500,
       body: JSON.stringify({ error: "Failed to download image" }),
       headers: {
         "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*", // Allow cross-origin requests
+        "Access-Control-Allow-Origin": "*",
       },
     };
   }
